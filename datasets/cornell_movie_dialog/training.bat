5c27d67ecc508031aec3b0965eef22afefd3d944
Muhammad Irfan Danish <6111007@gmail.com>
2019-05-20 00:46:39 +0500

    Add files via upload

---
+++ datasets/cornell_movie_dialog/training.bat
@@ -0,0 +1,5 @@
+call C:\Users\Danish\AppData\Local\Continuum\anaconda3\Scripts\activate.bat C:\Users\Danish\AppData\Local\Continuum\anaconda3
+cd ..\..\..
+python train.py --datasetdir=datasets\cornell_movie_dialog
+
+cmd /k
